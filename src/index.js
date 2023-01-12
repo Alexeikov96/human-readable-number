4b41692ab8ae56d3ee879daa3fe6add97553aed3
alexeikov96 <speedfaer96@gmail.com>
2023-01-13 00:54:16 +0300

    docs: add comments

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function toReadable (number) {
-  
+
+    /* 
+    
+    let nModule = String(Math.abs(n));
+    let reverseNumber = Number(nModule.split('').reverse().join(''));
+    
+    
+
+    const zero = 'zero';
+    const one = 'one';
+    const two = 'two';
+    const three = 'three';
+    const four = 'four';
+    const five = 'five';
+    const six = 'six';
+    const seven = 'seven';
+    const eight = 'eight';
+    const nine = 'nine';
+    const ten = 'ten';
+    const eleven = 'eleven';
+    const twelve = 'twelve';
+    const thirteen = 'thirteen';
+    const fourteen = 'fourteen';
+    const fifteen = 'fifteen';
+    const sixteen = 'sixteen';
+    const seventeen = 'seventeen';
+    const eighteen = 'eighteen';
+    const nineteen = 'nineteen';
+    const twenty = 'twenty';
+    const thirty = 'thirty';
+    const forty = 'forty'
+    const fifty= 'fifty'
+    const sixty= 'sixty'
+    const seventy = 'seventy'
+    const eighty = 'eighty'
+    const ninety = 'ninety'
+    const hundred = 'hundred'
+
+    let result = '';
+ */
+    
 }
