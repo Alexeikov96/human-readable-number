e0d481d5e120c0e69bc36702aa03fa57169171e1
alexeikov96 <speedfaer96@gmail.com>
2023-01-13 02:14:26 +0300

    fix: improved index.js

--- src/index.js
+++ src/index.js
@@ -1,43 +1,19 @@
 module.exports = function toReadable (number) {
-
-    /* 
-    
-    let nModule = String(Math.abs(n));
-    let reverseNumber = Number(nModule.split('').reverse().join(''));
-    
-    
-
-    const zero = 'zero';
-    const one = 'one';
-    const two = 'two';
-    const three = 'three';
-    const four = 'four';
-    const five = 'five';
-    const six = 'six';
-    const seven = 'seven';
-    const eight = 'eight';
-    const nine = 'nine';
-    const ten = 'ten';
-    const eleven = 'eleven';
-    const twelve = 'twelve';
-    const thirteen = 'thirteen';
-    const fourteen = 'fourteen';
-    const fifteen = 'fifteen';
-    const sixteen = 'sixteen';
-    const seventeen = 'seventeen';
-    const eighteen = 'eighteen';
-    const nineteen = 'nineteen';
-    const twenty = 'twenty';
-    const thirty = 'thirty';
-    const forty = 'forty'
-    const fifty= 'fifty'
-    const sixty= 'sixty'
-    const seventy = 'seventy'
-    const eighty = 'eighty'
-    const ninety = 'ninety'
-    const hundred = 'hundred'
-
+    let stringNumber = String(number);
     let result = '';
- */
-    
+    let items = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+/*     const hundred = 'hundred'; */
+
+    if(number === 0) {
+        result = 'zero';
+    } else if (number > 0 && number < 20) {
+        result = items[number];
+    } else if (number >= 20 && number < 100) {
+        result = `${tens[Number(stringNumber[0])]} ${items[Number(stringNumber[1])]}`;
+    } else { 
+        result = ((Number(stringNumber[1]) < 2) ? (`${items[Number(stringNumber[0])]} hundred ${items[Number(stringNumber.slice(1))]}`) : (`${items[Number(stringNumber[0])]} hundred ${tens[Number(stringNumber[1])]} ${items[Number(stringNumber[2])]}`));
+
+    }
+    return result.trim();
 }
