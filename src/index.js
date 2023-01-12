2fba2130029eb38fd3c144422c65651ec13c2086
alexeikov96 <speedfaer96@gmail.com>
2023-01-13 02:24:52 +0300

    fix: improved index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ module.exports = function toReadable (number) {
     let result = '';
     let items = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
     let tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-/*     const hundred = 'hundred'; */
 
     if(number === 0) {
         result = 'zero';
@@ -13,7 +12,7 @@ module.exports = function toReadable (number) {
         result = `${tens[Number(stringNumber[0])]} ${items[Number(stringNumber[1])]}`;
     } else { 
         result = ((Number(stringNumber[1]) < 2) ? (`${items[Number(stringNumber[0])]} hundred ${items[Number(stringNumber.slice(1))]}`) : (`${items[Number(stringNumber[0])]} hundred ${tens[Number(stringNumber[1])]} ${items[Number(stringNumber[2])]}`));
-
     }
+    
     return result.trim();
 }
